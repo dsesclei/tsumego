e4f4f2e0e09b53831aef594664c8e7faab472699
Alec Pierce <alec.m.pierce@gmail.com>
2016-11-02 12:11:10 -0500

    Updating to current files

--- backend/utils.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from . import serializers
-
-def jwt_response_payload_handler(token, user=None, request=None):
-    return {
-        'token': token,
-        'user': serializers.UserInfoSerializer(user, context={'request': request}).data
-    }
