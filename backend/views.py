73e1db0cb0d78209e93d6471574c4ee9618eba3d
Shawn Shan <jiahaoshan@126.com>
2016-11-15 01:51:14 -0600

    Fixed validation issue by not importing

--- backend/views.py
+++ backend/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError 
 from . import serializers
 from . import models
 
